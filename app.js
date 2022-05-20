7e62992d8ef9d16aa79e03bf3a38e8adfcd04c2e
DESKTOP-6HL68S6\RANJAN SUWAL <ranjan.suwal@cellcom.net.np>
2022-05-20 13:17:47 +0545

    pointed to live

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function () {
                 style="display:none;position:fixed;height:100%;width:100%;top:0;bottom:0;object-fit:contain;background:#fff;" 
                 src="https://cellpay-api.s3.ap-south-1.amazonaws.com/public/giphy.gif"/>
 
-            <iframe src="http://192.168.10.102/cellpay-web-sdk-server/index.htm" 
+            <iframe src="https://rxuswal.github.io/cellpay-web-sdk-server/index.htm" 
                     id="iframe" 
                     style="display:none;position:fixed;height:100%;width:100%;top:0;bottom:0;"/>`
 
@@ -69,7 +69,7 @@ window.addEventListener('load', function () {
 
 window.addEventListener('message', function (event) {
 
-    if (event.origin == 'http://192.168.10.102') {
+    if (event.origin == 'https://rxuswal.github.io') {
         document.getElementById('loading').style.display = "block"
         document.getElementById('iframe').style.display = "none"
 
@@ -81,7 +81,7 @@ window.addEventListener('message', function (event) {
 
         } else if ((event.data != "reload")) {
 
-            console.log("COMPLETE PAYMENT RESULT", event.data)
+            
 
             // FUNCTION ON CLIENT SIDE TO CATCH COMPLETE TXN DATA
             txnData(event.data)
